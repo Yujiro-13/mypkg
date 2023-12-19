3a66c290df4f2a87b4b8e9fa3e6d3c21be3f88fa
Yujiro Shito <s.yujiro613@gmal.com>
2023-12-19 18:20:37 +0900

    Rewrite callback object to talker class

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -6,20 +6,22 @@ from rclpy.node import Node      #ノードを実装するためのNodeクラス
 from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
 class Talker():
-    def __init__(self): # selfはオブジェクト
+    def __init__(self, node): # selfはオブジェクト
         self.pub = node.create_publisher(Int16, "countup", 10) # パブリッシャのオブジェクト作成
         self.n = 0
+        node.create_timer(0.5, self.cb) # タイマー設定
+        
+    def cb(self):          #17行目で定期実行されるコールバック関数
+        msg = Int16()  #メッセージの「オブジェクト」
+        msg.data = self.n   #msgオブジェクトの持つdataにnを結び付け
+        self.pub.publish(msg)        #pubの持つpublishでメッセージ送信
+        self.n += 1
 
-rclpy.init()
-node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-talker = Talker()                #talkerという「オブジェクト」を作成
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
 
-def cb():          #17行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-    talker.pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    talker.n += 1
-    
-node.create_timer(0.5, cb)  #タイマー設定
-rclpy.spin(node)            #実行（無限ループ）
+if __name__ == '__main__':
+    main()
